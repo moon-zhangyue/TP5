0a02fd4d63a7c05be46d1559d251f278e6f28197
wuning <wuning@jixiangkeji.com>
2017-06-26 18:07:32 +0800

    2017.06.26

--- application/config.php
+++ application/config.php
@@ -141,7 +141,9 @@ return [
     ],
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [],
+    'view_replace_str'       => [
+        '__STATIC__' => '/static',
+    ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
     'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
